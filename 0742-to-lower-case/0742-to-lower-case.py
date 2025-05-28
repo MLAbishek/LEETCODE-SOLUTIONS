97d5728aa687034dda5c56149c57d738de23622a
J. Abishek <152062416+MLAbishek@users.noreply.github.com>
2025-05-28 12:19:45 +0530

    Time: 2 ms (4.37%), Space: 17.8 MB (24.31%) - LeetHub

--- 0742-to-lower-case/0742-to-lower-case.py
+++ 0742-to-lower-case/0742-to-lower-case.py
@@ -1,4 +1,10 @@
 class Solution:
     def toLowerCase(self, s: str) -> str:
-        return s.lower()
-        
+        ans=""
+        for c in s:
+            if ord(c)>=ord('A') and ord(c)<=ord('Z'):
+                ans=ans+(chr(ord(c)+32))
+
+            else:
+                ans+=c
+        return ans
