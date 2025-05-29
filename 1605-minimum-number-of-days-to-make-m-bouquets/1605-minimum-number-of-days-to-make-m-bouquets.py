a8e83e674e8078806f7afa7d13f016cfbabee25b
J. Abishek <152062416+MLAbishek@users.noreply.github.com>
2025-05-29 11:00:48 +0530

    Time: 403 ms (31.97%), Space: 31.6 MB (34.3%) - LeetHub

---
+++ 1605-minimum-number-of-days-to-make-m-bouquets/1605-minimum-number-of-days-to-make-m-bouquets.py
@@ -0,0 +1,43 @@
+
+
+import math
+class Solution:
+    def perfect(self,arr,noboq,noflower,day):
+        count=0
+        curbloom=0
+        for i in range(len(arr)):
+            if arr[i]<=day:
+                count+=1
+            else:
+                curbloom+=math.floor(count/noflower)
+                count=0
+        curbloom+=math.floor(count/noflower)
+        
+        if curbloom>=noboq:
+            return True
+        else:
+            return False
+
+    def minDays(self, bloom: List[int], m: int, k: int) -> int:
+        ans=float('inf')
+        if m*k>len(bloom):
+            return -1
+        l=min(bloom)
+        r=max(bloom)
+        while(l<=r):
+            mid=(l+r)//2
+            if self.perfect(bloom,m,k,mid):
+                ans=min(ans,mid)
+                print(ans)
+                r=mid-1
+            else:
+                l=mid+1
+
+        return ans
+
+                
+
+        
+        
+
+        
