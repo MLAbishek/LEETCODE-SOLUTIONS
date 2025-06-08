60e13c151bae05a9b8e27bd353665681c71aaa6c
J. Abishek <152062416+MLAbishek@users.noreply.github.com>
2025-06-08 14:02:09 +0530

    Time: 150 ms (28.71%), Space: 23.9 MB (82.84%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.py
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.py
@@ -1,38 +1,12 @@
 class Solution:
-    def checkpresence(self,arr,r1,c1,R,C,target):
-        if(r1>=0 and r1<R and c1>=0 and c1<C):
-            #row
-            l=c1
-            r=C-1
-            while(l<=r):
-                mid=(l+r)//2
-                if arr[r1][mid]==target:
-                    return True
-                elif target<arr[r1][mid]:
-                    r=mid-1
-                else:
-                    l=mid+1
-            #col
-            l=r1
-            r=R-1
-            while(l<=r):
-                mid=(l+r)//2
-                if arr[mid][c1]==target:
-                    return True
-                elif target<arr[mid][c1]:
-                    r=mid-1
-                else:
-                    l=mid+1
-            return False
-
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        R=len(matrix)
-        C=len(matrix[0])
-        for r in range(R):
-            for c in range(C):
-                if(matrix[r][c]<=target and target<=matrix[r][C-1] and target<=matrix[R-1][c]):
-                    if self.checkpresence(matrix,r,c,R,C,target):
-                        return True
-        return False    
-                
+        row,col=0,len(matrix[0])-1
+        while(row<len(matrix) and col>=0):
+            if matrix[row][col]==target:
+                return True
+            elif matrix[row][col]<target:
+                row+=1
+            else:
+                col-=1
+        return False
         
