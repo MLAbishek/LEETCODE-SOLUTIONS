77b354399f23f776938dccb71d66bf34fcdda592
J. Abishek <152062416+MLAbishek@users.noreply.github.com>
2025-07-03 09:42:24 +0530

    Time: 3272 ms (19.71%), Space: 26.3 MB (5.4%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,32 +1,40 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        ans=""
-        lenans=0
-        if(len(s)==1):
-            return s
-        if(len(s)==2 and s[0]!=s[1]):
+        grid=[]
+        n=len(s)
+        start=0
+        end=0
+        for r in range(n):
+            grid.append([])
+            for c in range(n):
+                grid[r].append(0)
+        maxlen=0
+        for index in range(n):
+            row=0
+            col=index
+            while(row<n and col<n):
+                if s[row]==s[col]:
+                    if index>2:
+                        if grid[row+1][col-1]:
+                            if index+1>maxlen:
+                                maxlen=index+1
+                                start=row
+                                end=col
+                            grid[row][col]=1
+                    else:
+                        if index+1>maxlen:
+                            maxlen=index+1
+                            start=row
+                            end=col
+                        grid[row][col]=1
+                # print(row,col,s[row],s[col],grid[row][col])
+                row+=1
+                col+=1
+        # for i in grid:
+            # print(i)
+        if maxlen==1:
             return s[0]
-        for i in range(len(s)):
-            l,r=i,i
-            while(l>=0 and r<len(s) and s[l]==s[r]):
-                templen=r-l
-                if(templen > lenans):
-                    ans=s[l:r+1]
-                    lenans=r-l
-                l-=1
-                r+=1
+        # print(start,end,maxlen)
+        return s[start:end+1]
             
-            l,r=i,i+1
-            while(l>=0 and r<len(s) and s[l]==s[r]):
-                templen=r-l
-                if(templen > lenans):
-                    ans=s[l:r+1]
-                    lenans=r-l
-                l-=1
-                r+=1
-        if ans=="":
-            return s[0]
-        return ans
 
-
-        
