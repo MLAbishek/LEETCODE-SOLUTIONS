83897cc630829f70d85e5e594df805c8ff27732c
J. Abishek <152062416+MLAbishek@users.noreply.github.com>
2025-07-14 18:07:03 +0530

    Time: 72 ms (8.38%), Space: 41.6 MB (5.22%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -4,23 +4,31 @@
 #         self.val = val
 #         self.next = next
 class Solution:
+    def reverse(self,prev,curr):
+        if(curr==None):
+            return prev
+        nextnode=curr.next
+        curr.next=prev
+        return self.reverse(curr,nextnode)
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        stack=[]
-        ptr=head
-        while(ptr is not None):
-            stack.append(ptr)
-            ptr=ptr.next
-        
-        ptr=head
-        while(len(stack)!=0):
-            ele=stack.pop()
-            print(ele.val)
-            if ptr.val==ele.val:
-                ptr=ptr.next
-
+        slow=head
+        fast=head
+        while(fast.next is not None and fast.next.next is not None):
+            slow=slow.next
+            fast=fast.next.next
+        h1=head
+        h2=slow.next
+        checker=self.reverse(None,h2)
+        while(h1 is not None and checker is not None):
+            if h1.val==checker.val:
+                h1=h1.next
+                checker=checker.next
             else:
                 return False
         return True
+
+
+
 
         
 
