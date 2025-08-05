64ba3b20c8cdd3544984c38286a81f55e1c82e46
J. Abishek <152062416+MLAbishek@users.noreply.github.com>
2025-08-05 22:43:08 +0530

    Time: 0 ms (100%), Space: 17.9 MB (30.66%) - LeetHub

---
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.py
@@ -0,0 +1,10 @@
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+        
+        # while (b>0):
+        #     car=(a&b)<<1
+        #     a=a^b
+        #     b=car
+        # return a
+        return a+b
+        
