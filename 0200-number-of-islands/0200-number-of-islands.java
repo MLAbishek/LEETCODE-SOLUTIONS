0d75c08655750238883e870df67dc5afe71e96a2
J. Abishek <152062416+MLAbishek@users.noreply.github.com>
2025-08-27 10:38:37 +0530

    Time: 6 ms (22.95%), Space: 52.4 MB (16.77%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,27 +1,31 @@
 class Solution {
-    private static void checker(char[][] grid,int r,int c,int R,int C){
-        if(r<0 || c<0 || r>=R || c>=C || grid[r][c]=='0'){
-            return;
-        }
-        grid[r][c]='0';
-        checker(grid,r+1,c,R,C);
-        checker(grid,r,c+1,R,C);
-        checker(grid,r-1,c,R,C);
-        checker(grid,r,c-1,R,C);
-    }
     public int numIslands(char[][] grid) {
-        Scanner sc = new Scanner(System.in);
         int R=grid.length;
         int C=grid[0].length;
         int count=0;
+        boolean visited[][]=new boolean[R][C];
         for(int r=0;r<R;r++){
             for(int c=0;c<C;c++){
-                if(grid[r][c]=='1'){
-                    checker(grid,r,c,R,C);
+                if(grid[r][c]=='1' && !visited[r][c]){
+                    dfs(r,c,R,C,grid,visited);
                     count++;
                 }
             }
         }
         return count;
+    }
+    private static void dfs(int r,int c,int R,int C,char[][]grid, boolean[][] visited){
+        if(r<0 || r>=R || c<0 || c>=C  || grid[r][c]=='0' || visited[r][c]){
+            return;
+        }
+       
+        visited[r][c]=true;
+        int[][] diff={{1,0},{0,-1},{-1,0},{0,1}};
+        for(int i=0;i<4;i++){
+            int adjr=r+diff[i][0];
+            int adjc=c+diff[i][1];
+            dfs(adjr,adjc,R,C,grid,visited);
+
+        }
     }
 }
