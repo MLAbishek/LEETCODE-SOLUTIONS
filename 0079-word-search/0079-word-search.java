56f4130662650f420b6681e02eb877ee61d8f8dc
J. Abishek <152062416+MLAbishek@users.noreply.github.com>
2025-08-27 10:15:00 +0530

    Time: 260 ms (13.48%), Space: 45.1 MB (19.14%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,47 +1,39 @@
 class Solution {
-    private static boolean checker(char[][] board,int r,int c,int R,int C,String word,int index){
-        if(r<0 || r>=R || c<0 || c>=C || board[r][c]!=word.charAt(index)){
-            return false;
-        }
-        if(index==word.length()-1){
-            return true;
-        }
-        char curr = board[r][c];
-        board[r][c]='#';
-        int[][] diff = {{0,1},{1,0},{0,-1},{-1,0}};
-        for(int i=0;i<4;i++){
-            int dr=diff[i][0];
-            int dc=diff[i][1];
-            int adjr=r+dr;
-            int adjc=c+dc;
-            boolean found=checker(board,adjr,adjc,R,C,word,index+1);
-            if(found){
-                return true;
-            }
-
-        
-        }
-        board[r][c]=curr;
-        return false;
-
-    }
     public boolean exist(char[][] board, String word) {
-
         int R=board.length;
         int C=board[0].length;
         int index=0;
+        boolean visit[][]=new boolean[R][C];
         for(int r=0;r<R;r++){
             for(int c=0;c<C;c++){
-                if(word.charAt(index)==board[r][c]){
-                    boolean found=checker(board,r,c,R,C,word,index);
-                    if(found){
+                if(board[r][c]==word.charAt(0)){
+                    boolean ans=isValid(r,c,R,C,board,word,0,visit);
+                    if(ans)
                         return true;
-                    }
                 }
-                
             }
         }
         return false;
-
+    }
+    private static boolean isValid(int r,int c,int R,int C, char[][] board,String word,int index,boolean[][] visit){
+        if(index==word.length()-1 && word.charAt(index)==board[r][c]){
+            return true;
+        }
+        if(board[r][c]==word.charAt(index)){
+            int[][] diff={{1,0},{0,-1},{-1,0},{0,1}};
+            visit[r][c]=true;
+            for(int i=0;i<4;i++){
+                int adjr=r+diff[i][0];
+                int adjc=c+diff[i][1];
+                if(adjr>=0 && adjc>=0 && adjr<R && adjc<C && !visit[adjr][adjc]){
+                    boolean flag=isValid(adjr,adjc,R,C,board,word,index+1,visit);
+                    if(flag)
+                        return true;
+                }
+            }
+            visit[r][c]=false;
+            return false;
+        }
+        return false;
     }
 }
