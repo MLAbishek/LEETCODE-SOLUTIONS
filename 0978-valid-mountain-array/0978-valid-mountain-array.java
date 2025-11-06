66a85b36a4053aa8bb19e7e24357ee637e8c067c
J. Abishek <152062416+MLAbishek@users.noreply.github.com>
2025-11-06 10:16:15 +0530

    Time: 2 ms (72.15%), Space: 47.4 MB (5.21%) - LeetHub

--- 0978-valid-mountain-array/0978-valid-mountain-array.java
+++ 0978-valid-mountain-array/0978-valid-mountain-array.java
@@ -2,8 +2,7 @@ class Solution {
     public boolean validMountainArray(int[] arr) {
         boolean up=false;
         boolean down=false;
-        if(arr.length==1) return false;
-        if(arr.length==2 && arr[0]>=arr[1]) return false;
+        if(arr.length<3) return false;
         for(int i=1;i<arr.length;i++){
             if(arr[i-1]<arr[i]){
                 if(down) return false;
