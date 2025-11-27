e4a70730d477016a800267a78f67d32858b316d7
J. Abishek <152062416+MLAbishek@users.noreply.github.com>
2025-11-27 19:01:34 +0530

    Time: 4 ms (48.92%), Space: 54.5 MB (7.06%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,31 +1,29 @@
 class Solution {
+    private static void dfs(int R,int C,int r,int c,char[][] grid){
+        grid[r][c]='0';
+        int diff[][]={{-1,0},{0,-1},{1,0},{0,1}};
+        for(int i=0;i<4;i++){
+            int adjr=diff[i][0]+r;
+            int adjc=diff[i][1]+c;
+            if( adjr<R && adjc<C && adjr>=0 && adjc>=0 && grid[adjr][adjc]=='1'){
+                dfs(R,C,adjr,adjc,grid);
+            }
+        }
+
+    }
     public int numIslands(char[][] grid) {
+        int count=0;
         int R=grid.length;
         int C=grid[0].length;
-        int count=0;
-        boolean visited[][]=new boolean[R][C];
         for(int r=0;r<R;r++){
             for(int c=0;c<C;c++){
-                if(grid[r][c]=='1' && !visited[r][c]){
-                    dfs(r,c,R,C,grid,visited);
+                if(grid[r][c]=='1'){
                     count++;
+                    dfs(R,C,r,c,grid);
                 }
             }
         }
         return count;
-    }
-    private static void dfs(int r,int c,int R,int C,char[][]grid, boolean[][] visited){
-        if(r<0 || r>=R || c<0 || c>=C  || grid[r][c]=='0' || visited[r][c]){
-            return;
-        }
-       
-        visited[r][c]=true;
-        int[][] diff={{1,0},{0,-1},{-1,0},{0,1}};
-        for(int i=0;i<4;i++){
-            int adjr=r+diff[i][0];
-            int adjc=c+diff[i][1];
-            dfs(adjr,adjc,R,C,grid,visited);
 
-        }
     }
 }
