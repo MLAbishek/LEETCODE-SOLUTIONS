8c552df52544c669d409484c7cbe7e9e0807cc3e
J. Abishek <152062416+MLAbishek@users.noreply.github.com>
2026-01-07 22:32:09 +0530

    Time: 22 ms (1.55%), Space: 46.4 MB (8.47%) - LeetHub

---
+++ 0078-subsets/0078-subsets.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> ans=new ArrayList<>();
+        List<Integer> temp=new ArrayList<>();
+        helper(ans,0,nums.length,nums,temp);
+        return ans;
+
+    }
+    private static void helper(List<List<Integer>> ans,int i,int N,int[] nums,List<Integer> temp){
+        if(i==N){
+            ans.add(List.copyOf(temp));
+            return;
+        }
+        if(!ans.contains(temp)){
+            
+            temp.add(nums[i]);
+            helper(ans,i+1,N,nums,temp);
+            temp.remove(temp.size()-1);
+            helper(ans,i+1,N,nums,temp);
+        }
+        else{
+            return;
+        }
+
+    }
+}
