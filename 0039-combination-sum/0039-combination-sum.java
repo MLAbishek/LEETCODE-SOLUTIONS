49aa53170f8ce949b0215399f747348974f74d3e
J. Abishek <152062416+MLAbishek@users.noreply.github.com>
2026-01-13 14:22:26 +0530

    Time: 2 ms (95.07%), Space: 46 MB (24.94%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,24 +1,27 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] valarr, int target) {
-        List<List<Integer>> ans=new ArrayList<>();
-        List<Integer> curr=new ArrayList<>();
-        int N=valarr.length;
-        helper(0,target,curr,ans,valarr,N);
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> curr = new ArrayList<>();
+        int N = valarr.length;
+        helper(0, target, curr, ans, valarr, N);
         return ans;
     }
-    private static void helper(int index,int target,List<Integer> curr,List<List<Integer>> ans,int[] valarr,int N){
-        if(index==N){
-            if(target==0){
+
+    private static void helper(int index, int target, List<Integer> curr, List<List<Integer>> ans, int[] valarr,
+            int N) {
+        if (index == N) {
+            if (target == 0) {
                 ans.add(List.copyOf(curr));
             }
             return;
         }
-        if(target>=valarr[index]){
+        if (target >= valarr[index]) {
             curr.add(valarr[index]);
-            helper(index,target-valarr[index],curr,ans,valarr,N);
-            curr.remove(curr.size()-1);
-            
+            helper(index, target - valarr[index], curr, ans, valarr, N);
+            curr.remove(curr.size() - 1);
+
         }
-        helper(index+1,target,curr,ans,valarr,N);
+        helper(index + 1, target, curr, ans, valarr, N);
+        //This is where i made a mistake , we can move to the next index if the curr val is higher than the target value itself since the next value might be the right val for the target to achieve zero
     }
 }
