7f421688f8753f93cb2d30257dd316b163adf884
J. Abishek <152062416+MLAbishek@users.noreply.github.com>
2026-01-21 14:07:29 +0530

    Time: 224 ms (14.13%), Space: 46.9 MB (10.94%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -2,38 +2,35 @@ class Solution {
     public boolean exist(char[][] board, String word) {
         int R=board.length;
         int C=board[0].length;
-        int index=0;
-        boolean visit[][]=new boolean[R][C];
+        boolean visited[][]=new boolean[R][C];
         for(int r=0;r<R;r++){
             for(int c=0;c<C;c++){
-                if(board[r][c]==word.charAt(0)){
-                    boolean ans=isValid(r,c,R,C,board,word,0,visit);
-                    if(ans)
+                if(word.charAt(0)==board[r][c]){
+                    if(search(r,c,R,C,board,word,0,visited)){
                         return true;
+                    }
                 }
             }
         }
         return false;
     }
-    private static boolean isValid(int r,int c,int R,int C, char[][] board,String word,int index,boolean[][] visit){
-        if(index==word.length()-1 && word.charAt(index)==board[r][c]){
+    private static boolean search(int s,int e,int R,int C,char[][] board,String word,int index,boolean[][] visited){
+        if(index==word.length()-1 && word.charAt(index)==board[s][e]){
             return true;
         }
-        
-        int[][] diff={{1,0},{0,-1},{-1,0},{0,1}};
-        visit[r][c]=true;
+        visited[s][e]=true;
+        int diff[][]={{1,0},{0,-1},{-1,0},{0,1}};
         for(int i=0;i<4;i++){
-            int adjr=r+diff[i][0];
-            int adjc=c+diff[i][1];
-            if(adjr>=0 && adjc>=0 && adjr<R && adjc<C && !visit[adjr][adjc] && board[r][c]==word.charAt(index)){
-                boolean flag=isValid(adjr,adjc,R,C,board,word,index+1,visit);
-                if(flag)
+            int adjr=diff[i][0]+s;
+            int adjc=diff[i][1]+e;
+            if(adjr>=0 && adjr<R && adjc>=0 && adjc<C && !visited[adjr][adjc] && board[adjr][adjc]==word.charAt(index+1)){
+                if(search(adjr,adjc,R,C,board,word,index+1,visited)){
                     return true;
+                }
             }
         }
-        visit[r][c]=false;
+        visited[s][e]=false;
         return false;
-        }
-        
-    
+
+    }
 }
