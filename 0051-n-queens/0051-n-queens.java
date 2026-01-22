cbe59043b70df767938ef16ad79264fdb95823aa
J. Abishek <152062416+MLAbishek@users.noreply.github.com>
2026-01-22 10:31:17 +0530

    Time: 3 ms (45.91%), Space: 46.7 MB (35.73%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -1,54 +1,48 @@
 class Solution {
-    private static boolean safe(int r,int c,int R,int C,String[][] board){
-        for(int r1=r-1;r1>=0;r1--){
-            if(board[r1][c].equals("Q")){
-                return false;
-            }
-        }
-        for(int r1=r-1, c1=c-1;r1>=0 && c1>=0 ;r1--,c1--){
-            if(board[r1][c1].equals("Q")){
-                return false;
-            }
-        }
-        for(int r1=r-1,c1=c+1;r1>=0 && c1<C;r1--,c1++){
-            if(board[r1][c1].equals("Q")){
-                return false;
-            }
-        }
-        return true;
-    }
     public List<List<String>> solveNQueens(int n) {
-        int R=n;
-        int C=n;
-        String[][] board=new String[R][C];
-        for(int i=0;i<R;i++)
-            for(int j=0;j<C;j++)
-                board[i][j]=".";
-        List<List<String>> ans=new ArrayList<>();
-        helper(0,R,C,board,ans);
-        return ans;
-
-    }
-    private static void helper(int r,int R,int C,String[][] board,List<List<String>> ans){
-        if(r==R){
-            List<String> rows=new ArrayList<>();
-            for(int i=0;i<R;i++){
-                StringBuilder sb=new StringBuilder();
-                for(int j=0;j<C;j++){
-                    sb.append(board[i][j]);
-                }
-                rows.add(sb.toString());
-            }
-            ans.add(rows);
-            return;
-        }
-        for(int c=0;c<C;c++){
-            if(safe(r,c,R,C,board)){
-                board[r][c]="Q";
-                helper(r+1,R,C,board,ans);
+        int ned[]=new int[2*n-1];
+        int swd[]=new int[2*n-1];
+        int row[]=new int[n];
+        int col[]=new int[n];
+        List<List<String>> ans = new ArrayList<>();
+        String[][] board = new String[n][n];
+        for(int r=0;r<n;r++){
+            for(int c=0;c<n;c++){
                 board[r][c]=".";
             }
         }
+        helper(ned,swd,row,col,ans,board,0,n);
+        return ans;
+    }
+    private static void helper(int[] ned,int[] swd,int[] row,int[] col,List<List<String>> ans,String[][] board,int ind,int N){
+        if(ind==N){
+            List<String> temp=new ArrayList<>();
+            for(int r=0;r<N;r++){
+                String s="";
+                for(int c=0;c<N;c++){
+                    s+=board[r][c];
+                }
+                temp.add(s);
 
+            }
+            ans.add(temp);
+            return;
+        }
+        for(int c=0;c<N;c++){
+            
+            if(ned[ind+c]!=1 && swd[c-ind+N-1]!=1 && row[ind]!=1 && col[c]!=1 && !board[ind][c].equals("Q")){
+                ned[ind+c]=1;
+                swd[c-ind+N-1]=1;
+                row[ind]=1;
+                col[c]=1;
+                board[ind][c]="Q";
+                helper(ned,swd,row,col,ans,board,ind+1,N);
+                board[ind][c]=".";
+                ned[ind+c]=0;
+                swd[c-ind+N-1]=0;
+                row[ind]=0;
+                col[c]=0;
+            }
+        }
     }
 }
