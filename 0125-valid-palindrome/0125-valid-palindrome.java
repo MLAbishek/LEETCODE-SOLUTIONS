80b72b43c329fd033e2a3da7e6a9f4662528160e
J. Abishek <152062416+MLAbishek@users.noreply.github.com>
2026-02-03 08:42:56 +0530

    Time: 2 ms (98.84%), Space: 44.4 MB (74.67%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,39 @@
+class Solution {
+    private static boolean isAlphabetic(char c){
+        if('a'<=c && c<='z'){
+            return true;
+        }
+        if('A'<=c && c<='Z'){
+            return true;
+        }
+        if('0'<=c && c<='9'){
+            return true;
+        }
+        return false;
+    }
+    public boolean isPalindrome(String s) {
+        int l = 0;
+        int r = s.length() - 1;
+        if (s.equals(" "))
+            return true;
+        while (l <= r) {
+            char left = s.charAt(l);
+            char right = s.charAt(r);
+            if (!isAlphabetic(left) || !isAlphabetic(right)) {
+                if (!isAlphabetic(left)) {
+                    l++;
+                } else {
+                    r--;
+                }
+            } else {
+                if (Character.toLowerCase(s.charAt(l)) != Character.toLowerCase(s.charAt(r))) {
+                    return false;
+                } else {
+                    l++;
+                    r--;
+                }
+            }
+        }
+        return true;
+    }
+}
