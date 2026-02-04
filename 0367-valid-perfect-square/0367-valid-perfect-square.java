3b5c3f43c64eb76534675900b1f5e3348ed0d6ab
J. Abishek <152062416+MLAbishek@users.noreply.github.com>
2026-02-04 11:53:37 +0530

    Time: 1 ms (20.38%), Space: 42 MB (71.12%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.java
+++ 0367-valid-perfect-square/0367-valid-perfect-square.java
@@ -1,19 +1,12 @@
 class Solution {
     public boolean isPerfectSquare(int num) {
-        long left=1;
-        long right=num;
-        while(left<=right){
-            long mid=(left+right)/2;
-            long sq=mid*mid;
-            if(sq==num){
-                return true;
-            }
-            else if(sq<num){
-                left=mid+1;
-            }
-            else{
-                right=mid-1;
-            }
+        int odd=1;
+        while(num>0){
+            num-=odd;
+            odd+=2;
+        }
+        if(num==0){
+            return true;
         }
         return false;
     }
