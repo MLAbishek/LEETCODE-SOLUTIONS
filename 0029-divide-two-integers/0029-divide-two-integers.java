f155d97bca6ef7dd736d0ba9e4e9350d7d423e10
J. Abishek <152062416+MLAbishek@users.noreply.github.com>
2026-02-17 19:22:44 +0530

    Time: 1 ms (90.26%), Space: 42.9 MB (12.27%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.java
+++ 0029-divide-two-integers/0029-divide-two-integers.java
@@ -1,25 +1,25 @@
 class Solution {
     public int divide(int dividend, int divisor) {
-
-        if (dividend == Integer.MIN_VALUE && divisor == -1)
+        if(dividend==Integer.MIN_VALUE && divisor==-1){
             return Integer.MAX_VALUE;
-
-        boolean sign = (dividend >= 0) == (divisor >= 0);
-
-        long d = Math.abs((long) dividend);
-        long n = Math.abs((long) divisor);
-
-        int ans = 0;
-
-        while (d >= n) {
-            int count = 0;
-            while (d >= (n << (count + 1))) {
+        }
+        boolean sign=true;
+        if((dividend<0 && divisor>0) || (dividend>0 && divisor<0)){
+            sign=false;
+        }
+        int ans=0;
+        long d=(Math.abs((long)dividend));
+        long n=(Math.abs((long)divisor));
+        while(d>=n){
+            int count=0;
+            while(d>=(n<<count+1)){
                 count++;
             }
-            ans += (1 << count);
-            d -= (n << count);
+            ans+=(1<<count);
+            d-=(n<<count);
         }
-
-        return sign ? ans : -ans;
+        if(!sign)
+            return -ans;
+        return ans;
     }
 }
