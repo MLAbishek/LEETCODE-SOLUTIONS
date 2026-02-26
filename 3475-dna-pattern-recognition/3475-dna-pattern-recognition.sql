03ac90c155fa55255514b4d457daa053b217134e
J. Abishek <152062416+MLAbishek@users.noreply.github.com>
2026-02-26 11:38:36 +0530

    Time: 1084 ms (5.04%), Space: 0B (100%) - LeetHub

---
+++ 3475-dna-pattern-recognition/3475-dna-pattern-recognition.sql
@@ -0,0 +1,31 @@
+SELECT
+    sample_id,
+    dna_sequence,
+    species,
+
+    -- starts with ATG
+    CASE 
+        WHEN dna_sequence LIKE 'ATG%' THEN 1 
+        ELSE 0 
+    END AS has_start,
+
+    -- ends with TAA, TAG, or TGA
+    CASE 
+        WHEN dna_sequence REGEXP 'TAA$|TAG$|TGA$' THEN 1
+        ELSE 0
+    END AS has_stop,
+
+    -- contains ATAT
+    CASE 
+        WHEN dna_sequence LIKE '%ATAT%' THEN 1
+        ELSE 0
+    END AS has_atat,
+
+    -- contains at least 3 consecutive G
+    CASE 
+        WHEN dna_sequence REGEXP 'G{3,}' THEN 1
+        ELSE 0
+    END AS has_ggg
+
+FROM Samples
+ORDER BY sample_id;
