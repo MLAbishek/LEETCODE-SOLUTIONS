8c05f9a643c3d59769198711bf551ac1ca4f4ecf
J. Abishek <152062416+MLAbishek@users.noreply.github.com>
2026-03-02 23:43:18 +0530

    Time: 1 ms (60.26%), Space: 48.7 MB (24.11%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,21 +1,26 @@
 class Solution {
-    public int trap(int[] height) {
-        int l=0,r=height.length-1;
-        int maxleft=height[l];
-        int maxright=height[r];
-        int ans=0;
-        while(l<r){
-            if(height[l]<height[r]){
-                l++;
-                maxleft=Math.max(maxleft,height[l]);
-                ans+=maxleft-height[l];
-            }
-            else{
-                r--;
-                maxright=Math.max(maxright,height[r]);
-                ans+=maxright-height[r];
+    public int trap(int[] arr) {
+        int total=0;
+        int N=arr.length;
+        int prefix[]=new int[N];
+        prefix[0]=arr[0];
+        for(int i=1;i<N;i++){
+            prefix[i]=Math.max(arr[i],prefix[i-1]);
+        }
+        
+        int postfix[]=new int[N];
+        postfix[N-1]=arr[N-1];
+        for(int i=N-2;i>=0;i--){
+            postfix[i]=Math.max(arr[i],postfix[i+1]);
+        }
+
+        for(int i=0;i<N-1;i++){
+            int lmax=prefix[i];
+            int rmax=postfix[i];
+            if(lmax>arr[i] && rmax>arr[i]){
+                total+=Math.min(lmax,rmax)-arr[i];
             }
         }
-        return ans;
+        return total;
     }
 }
