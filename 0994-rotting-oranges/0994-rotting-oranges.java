7f80c0ea710f1709202b0620f8e36101fc78fd05
J. Abishek <152062416+MLAbishek@users.noreply.github.com>
2026-03-17 09:08:52 +0530

    Time: 2 ms (90.45%), Space: 44.9 MB (5.41%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,47 +1,64 @@
 class Solution {
-    private static int BFS(int R,int C,int[][] grid,int fresh,Queue<int[]> q){
+    private static int dfs(Queue<int[]> q, boolean[][] visited,int[][] grid,int fresh,int R,int C){
         int days=0;
-        while(!q.isEmpty() && fresh>0){
-            int[][] diff={{1,0},{0,1},{-1,0},{0,-1}};
-            int size=q.size();
-            for(int i=0;i<size;i++){
-                int[] co=q.poll();
-                int r=co[0];
-                int c=co[1];
-                for(int d=0;d<4;d++){
-                    int adjr=r+diff[d][0];
-                    int adjc=c+diff[d][1];
-                    if(adjr<R && adjr>=0 && adjc<C && adjc>=0 && grid[adjr][adjc]==1){
-                        q.add(new int[]{adjr,adjc});
+        
+        while(q.size()>0){
+            boolean rot=false;
+            int qsize=q.size();
+            while(qsize-- >0){
+                int[] point=q.poll();
+                int r=point[0];
+                int c=point[1];
+                
+                int diff[][]={{0,1},{1,0},{-1,0},{0,-1}};
+                for(int i=0;i<4;i++){
+                    int adjr=diff[i][0]+r;
+                    int adjc=diff[i][1]+c;
+                    if(adjr>=0 && adjr<R && adjc>=0 && adjc<C && !visited[adjr][adjc] && grid[adjr][adjc]==1){
+                        visited[adjr][adjc]=true;
                         grid[adjr][adjc]=2;
                         fresh--;
-                        
+                        q.add(new int[] {adjr,adjc});
+                        rot=true;
                     }
-                }
 
+
+                }
             }
-            days++;
+            if(rot)
+                days++;
+
         }
         if(fresh!=0){
             return -1;
         }
-        return days;
-    } 
+        else{
+            return days;
+        }
+    }
     public int orangesRotting(int[][] grid) {
-        int fresh=0;
         int R=grid.length;
         int C=grid[0].length;
-        Queue<int[]> q=new LinkedList<>();
+        int fresh=0;
+        Queue<int[]> q = new LinkedList<>();
+        boolean visited[][]=new boolean[R][C];
         for(int r=0;r<R;r++){
             for(int c=0;c<C;c++){
                 if(grid[r][c]==1){
                     fresh++;
                 }
                 if(grid[r][c]==2){
-                    q.add(new int[]{r,c});
+                    visited[r][c]=true;
+                    q.add(new int[] {r,c});
                 }
             }
         }
-        return BFS(R,C,grid,fresh,q);
+        return dfs(q,visited,grid,fresh,R,C);
+        
+
+
+
+
+
     }
 }
