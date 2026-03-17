4d99a3effc2086399ca2b5c0ee29e55294973b12
J. Abishek <152062416+MLAbishek@users.noreply.github.com>
2026-03-17 10:54:40 +0530

    Time: 4 ms (73.26%), Space: 45.4 MB (30.88%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,24 +1,48 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        Stack<Integer> st = new Stack<>();
-        int ans[]=new int[nums2.length];
+        Stack<Integer> s = new Stack<>();
+        int N=nums2.length;
         HashMap<Integer,Integer> map = new HashMap<>();
-        for(int i=nums2.length-1;i>=0;i--){
-            while(!st.isEmpty() && st.peek()<=nums2[i]){
-                st.pop();
+        for(int i=N-1;i>=0;i--){
+            if(s.isEmpty()){
+                
+                map.put(nums2[i],-1);
+                s.push(nums2[i]);
+                nums2[i]=-1;
+                
             }
-            if(st.isEmpty()){
-                ans[i]=-1;
+            else if(s.peek()>nums2[i]){
+                
+                map.put(nums2[i],s.peek());
+                s.push(nums2[i]);
+                nums2[i]=s.peek();
+                
+
             }
             else{
-                ans[i]=st.peek();
+                while(!s.isEmpty() && s.peek()<nums2[i]){
+                    s.pop();
+                }
+                if(!s.isEmpty()){
+                    
+                    map.put(nums2[i],s.peek());
+                    s.push(nums2[i]);
+                    nums2[i]=s.peek();
+
+                    
+                }
+                else{
+                    
+                    map.put(nums2[i],-1);
+                    s.push(nums2[i]);
+                    nums2[i]=-1;
+                }
             }
-            st.add(nums2[i]);
-            map.put(nums2[i],i);
         }
         for(int i=0;i<nums1.length;i++){
-            nums1[i]=ans[map.get(nums1[i])];
+            nums1[i]=map.get(nums1[i]);
         }
         return nums1;
+
     }
 }
