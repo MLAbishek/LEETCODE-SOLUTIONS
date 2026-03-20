a6c83b55c8f4368a5b29d642ea1605577a209acb
J. Abishek <152062416+MLAbishek@users.noreply.github.com>
2026-03-20 16:50:43 +0530

    Time: 0 ms (100%), Space: 44.7 MB (6.6%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.java
+++ 0061-rotate-list/0061-rotate-list.java
@@ -9,40 +9,48 @@
  * }
  */
 class Solution {
-    public ListNode rotateRight(ListNode head, int k) {
-        int len=0;
-        if(head==null)
-            return null;
-        if(k==0)
-            return head;
-        ListNode temp=head;
-        while(temp!=null){
-            temp=temp.next;
-            len++;
-        }
-        if(len==1)
-            return head;
-        k=k%len;
-        int stop=Math.abs(len-k);
-        System.out.println(stop);
-        if(stop==0)
-            return head;
-        int count=0;
-        ListNode p1,p2;
-        p1=head;
-        for(int i=0;i<stop-1 && p1!=null ;i++){
-            p1=p1.next;
-        }
-        p2=p1.next;
-        ListNode newhead=p2;
-        p1.next=null;
-        if(p2==null)
-            return head;
-        while(p2.next!=null){
-            p2=p2.next;
-        }
-        p2.next=head;
-        return newhead;
+    private static ListNode findk(ListNode head,int tot,int k){
+        while(head.next!=null && --tot >k){
+            head=head.next;
 
+        }
+        return head;
+    }
+    private static ListNode reverse(ListNode head){
+        ListNode prev=null;
+        ListNode curr=head;
+        while(curr!=null){
+            ListNode nextn=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=nextn;
+        }
+        return prev;
+    }
+    public ListNode rotateRight(ListNode head, int k) {
+        int count=0;
+        ListNode ctr=head;
+        if(head==null){
+            return head;
+        }
+        while(ctr!=null){
+            count++;
+            ctr=ctr.next;
+        }
+        k=k%count;
+        ListNode prev=null;
+        ListNode temp=head;
+        ListNode kth=findk(temp,count,k);
+        if(temp==kth && k>=count){
+            ListNode ans=reverse(head);
+            return ans;
+        }
+        ListNode nextn=kth.next;
+        kth.next=null;
+        reverse(temp);
+        ListNode shalf=reverse(nextn);
+        temp.next=shalf;
+        ListNode finall=reverse(kth);
+        return finall;
     }
 }
