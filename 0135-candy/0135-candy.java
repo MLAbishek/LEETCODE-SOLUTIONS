8ed0335bb8ae1fc9d0a4e80dd7600d1fff4ca83a
J. Abishek <152062416+MLAbishek@users.noreply.github.com>
2026-03-26 23:13:20 +0530

    Time: 3 ms (86.26%), Space: 47.7 MB (43.97%) - LeetHub

---
+++ 0135-candy/0135-candy.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int candy(int[] ratings) {
+        int N=ratings.length;
+        int arr[]=new int[N];
+        Arrays.fill(arr,1);
+        for(int i=1;i<N;i++){
+            if(ratings[i]>ratings[i-1]){
+                arr[i]=arr[i-1]+1;
+            }
+        }
+
+        for(int i=N-2;i>=0; i--){
+            if(ratings[i]>ratings[i+1]){
+                arr[i]=Math.max(arr[i],arr[i+1]+1);
+            }
+        }
+        int ans=0;
+        for(int i:arr){
+            ans+=i;
+        }
+        return ans;
+
+    }
+}
