5cc150f0f77d0141a0f4dcaa827707a63ef689fe
J. Abishek <152062416+MLAbishek@users.noreply.github.com>
2026-06-08 14:05:19 +0530

    Time: 0 ms (100%), Space: 42.7 MB (58.61%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,22 +1,34 @@
 class Solution {
     public int rob(int[] nums) {
-        int N=nums.length;
-        if (N == 1)
-            return nums[0];
-        if (N == 2)
-            return Math.max(nums[0], nums[1]);
-        return Math.max(helper(0,N-2,nums),helper(1,N-1,nums));
-    }
-
-    private static int helper(int s,int e,int[] nums) {
-        int N =e-s+1;
-        int dp[] = new int[N];
-        
-        dp[0] = nums[s];
-        dp[1] = Math.max(nums[s+1], nums[s]);
-        for (int i = 2; i < N; i++) {
-            dp[i] = Math.max(dp[i - 1], nums[s+i] + dp[i - 2]);
+        int n=nums.length;
+        if(n==1) return nums[0];
+        int temp1[] = new int[n-1];
+        int temp2[] = new int[n-1];
+        for(int i=0;i<n;i++){
+            if(i!=0){
+                temp1[i-1] = nums[i];
+            }
+            if(i!=n-1){
+                temp2[i] = nums[i];
+            }
         }
-        return dp[N - 1];
+        return Math.max(helper(temp1),helper(temp2));
     }
+    private static int helper(int[] nums){
+        int n=nums.length;
+        int prev2=0;
+        int prev = nums[0];
+        for(int i=1;i<n;i++){
+            int pick = nums[i];
+            if(i>1){
+                pick += prev2;
+            }
+            int not_pick = prev;
+            int curr = Math.max(pick,not_pick);
+            prev2=prev;
+            prev=curr;
+        }
+        return prev;
+    }
+    
 }
