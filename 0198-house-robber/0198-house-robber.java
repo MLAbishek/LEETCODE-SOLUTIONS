8015fbdba40780ab353f93266f79fb8e256bb00c
J. Abishek <152062416+MLAbishek@users.noreply.github.com>
2026-06-08 11:24:17 +0530

    Time: 0 ms (100%), Space: 42.7 MB (72.85%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,20 +1,32 @@
 class Solution {
-    private static int helper(int[] arr,int ind,int[] dp){
-        if(ind<0){
-            return 0;
-        }
-        if(dp[ind]!=-1){
-            return dp[ind];
-        }
-        int left=helper(arr,ind-1,dp);
-        int right=arr[ind]+helper(arr,ind-2,dp);
-        dp[ind] = Math.max(left,right);
-        return dp[ind];
-    }
+    // private static int helper(int[] arr,int ind,int[] dp){
+    //     if(ind<0){
+    //         return 0;
+    //     }
+    //     if(dp[ind]!=-1){
+    //         return dp[ind];
+    //     }
+    //     int left=helper(arr,ind-1,dp);
+    //     int right=arr[ind]+helper(arr,ind-2,dp);
+    //     dp[ind] = Math.max(left,right);
+    //     return dp[ind];
+    // }
     public int rob(int[] nums) {
         int n=nums.length;
         int dp[] = new int[n];
-        Arrays.fill(dp,-1);
-        return helper(nums,n-1,dp);
+        // Arrays.fill(dp,-1);
+        if(n==1 ){
+            return nums[0];
+        }
+        if(n==2){
+            return Math.max(nums[0],nums[1]);
+        }
+        dp[0]=nums[0];
+        dp[1]=Math.max(nums[0],nums[1]);
+        for(int i=2;i<n;i++){
+            dp[i]=Math.max(dp[i-2]+nums[i],dp[i-1]);
+        }
+        return dp[n-1];
+        
     }
 }
