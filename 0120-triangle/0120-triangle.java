8590c8bc40d81d8fd11bee4e4d487ce42db0b89e
J. Abishek <152062416+MLAbishek@users.noreply.github.com>
2026-06-10 14:01:05 +0530

    Time: 1 ms (99.88%), Space: 45.5 MB (60.47%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,14 +1,31 @@
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
         int R=triangle.size();
-        for(int r=R-2;r>=0;r--){
-            int C=triangle.get(r).size();
-            for(int c=C-1;c>=0;c--){
-                int min=Math.min(triangle.get(r+1).get(c),triangle.get(r+1).get(c+1));
-                int curval=triangle.get(r).get(c);
-                triangle.get(r).set(c,min+curval);
-            }
+        int C=0;
+        for(int r=0;r<R;r++){
+            C=Math.max(C,triangle.get(r).size());
+            
         }
-        return triangle.get(0).get(0);
+   
+        int[][] dp = new int[R][C];
+        for(int r=0;r<R;r++){
+            Arrays.fill(dp[r],Integer.MAX_VALUE);
+        }
+        return path(0,0,R,triangle,dp);
+
+    }
+    private static int path(int r,int c,int R,List<List<Integer>> mat,int[][] dp){
+        if(r==R){
+            return 0;
+        }
+        if(c >= mat.get(r).size()){
+            return Integer.MAX_VALUE;
+        }
+        if(dp[r][c]!=Integer.MAX_VALUE){
+            return dp[r][c];
+        }
+        int left=path(r+1,c,R,mat,dp);
+        int right=path(r+1,c+1,R,mat,dp);
+        return dp[r][c]=mat.get(r).get(c)+Math.min(left,right);
     }
 }
