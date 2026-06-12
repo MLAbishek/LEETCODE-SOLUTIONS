a0fe7ff7053511bf74c0c5e34eb9b77273d30e31
J. Abishek <152062416+MLAbishek@users.noreply.github.com>
2026-06-12 11:17:24 +0530

    Time: 6 ms (59.46%), Space: 26.3 MB (93.4%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -1,20 +1,18 @@
 class Solution:
-    def helper(self,arr,l,r):
-        while(l<=r):
-            arr[l],arr[r]=arr[r],arr[l]
+    def reverse(self,arr,l,r):
+        while(l<r):
+            arr[l],arr[r] = arr[r],arr[l]
             l+=1
             r-=1
     
-    def rotate(self, nums: List[int], k: int) -> None:
+    def rotate(self, arr: List[int], k: int) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
-        
-        k=k%len(nums)
-        rem=(len(nums)-k)
-        
-        self.helper(nums,0,rem-1)
-        self.helper(nums,rem,len(nums)-1)
-        self.helper(nums,0,len(nums)-1)
-        return nums
+        n=len(arr)
 
+        k=k%n
+        self.reverse(arr,0,n-1-k)
+        self.reverse(arr,n-k,n-1)
+        self.reverse(arr,0,n-1)
+        
